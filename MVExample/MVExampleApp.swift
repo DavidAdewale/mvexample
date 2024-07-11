b3beeaf909dc039ee96573e681f0f2bd8d699743
DavidAdewale <adewaledavidola@gmail.com>
2024-07-11 11:36:52 +0100

    Initial Commit

---
+++ MVExample/MVExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVExampleApp.swift
+//  MVExample
+//
+//  Created by David Adewale on 11/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct MVExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
